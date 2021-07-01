f117f686011d1834ee9e56f7e725e60162a2c9f6
Robert Eves <roberteves92@gmail.com>
2021-07-01 09:34:14 +0100

    change flight log name space

--- FSUIPCHelper/FSData/Radios.cs
+++ FSUIPCHelper/FSData/Radios.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using FSUIPC;
-using FSUIPCHelper.Flight;
 using FSUIPCHelper.Logging;
 
 namespace FSUIPCHelper.FSData
--- FSUIPCHelper/Logging/FlightLog.cs
+++ FSUIPCHelper/Logging/FlightLog.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace FSUIPCHelper.Flight
+namespace FSUIPCHelper.Logging
 {
     /// <summary>
     /// CORE/FLIGHT: Methods and Variables for Flight Log management
